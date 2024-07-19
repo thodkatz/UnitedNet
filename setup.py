a8c8d163002673037e69f864bf08f1e85de08d0e
Theodoros Katzalis <theodoros.katzalis@cern.ch>
2024-07-19 10:33:48 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='UnitedNet',
+    version="0.1.0",
+    packages=['src', 'src.configs'],
+    url='',
+    license='',
+    author='thodkatz',
+    author_email='',
+    description='',
+    install_requires=[
+        "matplotlib",
+        "ipykernel",
+        "scanpy",
+        "numpy",
+        "pandas"    ]
+)
